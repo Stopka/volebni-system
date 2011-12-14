f74408efec9dbb6ca0877f526cad4e564f6c6510
Stepan.S@gmail.com <Stepan.S@gmail.com@ce3ee516-e480-618d-3937-0c9484288bce>
2011-12-14 12:13:28 +0000

    Uprava db zpět

--- src/reg_db.sql
+++ src/reg_db.sql
@@ -32,6 +32,7 @@ CREATE  TABLE IF NOT EXISTS `mydb`.`User` (
   `surname` VARCHAR(45) NOT NULL ,
   `login` VARCHAR(45) NOT NULL ,
   `email` VARCHAR(128) NOT NULL ,
+  `password` VARCHAR(45) NOT NULL ,
   PRIMARY KEY (`id`) )
 ENGINE = InnoDB;
 
@@ -62,7 +63,6 @@ CREATE  TABLE IF NOT EXISTS `mydb`.`Participation` (
   `id` BIGINT NOT NULL AUTO_INCREMENT ,
   `Action_id` BIGINT NOT NULL ,
   `User_id` BIGINT NOT NULL ,
-  `password` VARCHAR(45) NOT NULL ,
   PRIMARY KEY (`id`) ,
   INDEX `fk_Participation_Action1` (`Action_id` ASC) ,
   INDEX `fk_Participation_User1` (`User_id` ASC) ,
